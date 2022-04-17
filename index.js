dc75b8a7e703af4d144cb058eb33a5d6b93efe96
Liridon Loku <liridonloku@gmail.com>
2022-04-17 13:50:22 +0200

    Refactor index.js

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ const port = 8080;
 
 const server = http.createServer((req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
-  if (url.pathname === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    fs.createReadStream("./index.html").pipe(res);
-  } else if (url.pathname === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    fs.createReadStream("./about.html").pipe(res);
-  } else if (url.pathname === "/contact-me") {
-    res.writeHead(200, { "content-type": "text/html" });
-    fs.createReadStream("./contact-me.html").pipe(res);
-  } else {
-    res.writeHead(200, { "content-type": "text/html" });
-    fs.createReadStream("./404.html").pipe(res);
+  res.writeHead(200, { "content-type": "text/html" });
+  switch (url.pathname) {
+    case "/":
+      fs.createReadStream("./index.html").pipe(res);
+      break;
+    case "/about":
+      fs.createReadStream("./about.html").pipe(res);
+      break;
+    case "/contact-me":
+      fs.createReadStream("./contact-me.html").pipe(res);
+      break;
+    default:
+      fs.createReadStream("./404.html").pipe(res);
+      break;
   }
 });
 
